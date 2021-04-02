7e562deb500555a3d2e33b84fd6a82ffe19d3cdb
东风谷早苗 <532109377@qq.com>
2021-04-02 17:58:29 +0800

    项目再启动！

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="goods">
+    {{ goods }}
+  </div>
+</template>
+
+<script>
+import GoodsListItem from "@/components/content/goods/GoodsListItem"
+
+export default {
+  name: "GoodsList",
+  components: {
+    GoodsListItem
+  },
+  props: {
+    goods: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.goods {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  justify-content: center;
+}
+</style>
